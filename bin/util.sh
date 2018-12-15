755c127ec40a228f5f07c28f368337780bce15b6
Shawn Boyette <shawn@firepear.net>
2018-12-15 02:10:53 -0700

    wip arch control setup

--- bin/util.sh
+++ bin/util.sh
@@ -7,3 +7,55 @@ shownotice () {
     echo " ${1}"
     echo
 }
+
+affirmative() {
+    RESPONSE=""
+    read -r -p "${1} (y/N)? " RESPONSE
+    if [[ "${RESPONSE^^}" ~= ^Y ]]; then
+        echo 0
+    else
+        echo 1
+    fi
+}
+
+gutcheck() {
+    if [[ "${2}" = "" ]]; then
+        PROMPT="${1}: "
+    else
+        PROMPT="${1} (default is '${2})': "
+    fi
+
+    ASKAGAIN="n"
+    while [[ "${ASKAGAIN}^^" !~ ^Y ]]
+    do
+        read -r -p "${PROMPT}: " GUTCHECK
+        # return the default if one was provided and the user just hit
+        # enter
+        if [[ "${2}" != "" ]] && [[ "${GUTCHECK}" == "" ]]; then
+            echo "${2}"
+            return
+        fi
+        echo -n "You entered '${GUTCHECK}'. Is this correct (y/n)? "
+        read -r ASKAGAIN
+    done
+    echo ${GUTCHECK}
+}
+
+terminate() {
+    echo
+    echo "${1}. Terminating install."
+    echo
+    exit 1
+}
+
+findmac() {
+    IFACES=( $(ls /sys/class/net) )
+    for if in $IFACES
+    do
+        if [[ ${if} != "lo" ]]; then
+            IFNAME=${if}
+        fi
+    done
+    MAC=$( cat /sys/class/net/${IFNAME}/address )
+    echo "${IFNAME} ${MAC}"
+}
